ee69d85819c3475b86bd54b90404e67a1e9bc94f
orhan <orhantrkmn749@gmail.com>
2023-12-24 15:10:38 +0300

    Initial commit

---
+++ src/main/java/com/example/springRestApiPraticeApp/SpringRestApiPraticeAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springRestApiPraticeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiPraticeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApiPraticeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springRestApiPraticeApp/SpringRestApiPraticeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springRestApiPraticeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiPraticeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
